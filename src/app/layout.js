db864cc2027ce250a717280ca6b11b5cc64afb77
harshii04 <harshvardhan.agarwal2020a@vitstudent.ac.in>
2023-09-28 15:01:58 +0530

    html classname removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,7 +35,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
       <body suppressHydrationWarning={true} className={satoshi.className}>
         {children}
       </body>
