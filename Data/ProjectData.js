c483051c2a4551dd7fb94b654adb947cfd2963cc
Harshvardhan Agarwal <72592243+harshii04@users.noreply.github.com>
2023-10-03 10:28:01 +0530

    Update ProjectData.js

--- Data/ProjectData.js
+++ Data/ProjectData.js
@@ -4,7 +4,7 @@ const projectData = {
       id: 0,
       role: "Product Designer",
       tools: "Figma, Notion",
-      timeline: "June 2022",
+      timeline: "June 2023",
       description: "A plant and it's equipment delivery app concept.",
       content: `I created a plant delivery app concept that simplifies choosing the right plant by showing their specific care needs. After ordering, users can keep track of their plants in the 'My Garden' section, where they can see when to water them or protect them from too much sunlight.`,
       link: "https://medium.com/@harsh_youx/sprout-concept-app-for-buying-plants-seeds-gardening-equipment-etc-fd78b2a83a18",
@@ -13,7 +13,7 @@ const projectData = {
       id: 1,
       role: "Product Designer",
       tools: "Figma, Notion",
-      timeline: "June 2023",
+      timeline: "June 2022",
       description: "Simplify group food ordering with quick restaurant voting.",
       content:
         "This app aims to make group food ordering easier. It tackles the common problem of taking too long to decide what to order by introducing a voting system to choose the restaurant quickly.",
@@ -23,7 +23,7 @@ const projectData = {
       id: 2,
       role: "Product Designer",
       tools: "Figma, Notion",
-      timeline: "June 2023",
+      timeline: "Jan 2022",
       description:
         "Designed trivia game for Dive platform to help users reduce the burnout during remote work.",
       content:
