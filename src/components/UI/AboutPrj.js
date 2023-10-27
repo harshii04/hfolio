14ae0e4327ece63e7347f4d47004633defcc39cc
harshii04 <harshvardhan.agarwal2020a@vitstudent.ac.in>
2023-10-27 23:54:24 +0530

    UI changes

--- src/components/UI/AboutPrj.js
+++ src/components/UI/AboutPrj.js
@@ -8,7 +8,7 @@ const AboutPrj = ({ role, tools, timeline, description, content, link }) => {
       <div className="md:flex md:flex-row flex-col items-start justify-between md:pt-8 pt-6 md:gap-24 gap-8">
         <div className="flex flex-row justify-between items-start md:flex-col gap-8 md:pb-0 pb-6 flex-wrap md:w-[20%]">
           <div className="flex flex-col gap-2">
-            <p className="font-normal text-xs leading-heading text-text-secondary">
+            <p className="font-bold text-xs leading-heading text-text-secondary">
               MY ROLE
             </p>
             <p className="text-text-primary leading-body md:text-lg text-base font-medium w-36">
@@ -16,7 +16,7 @@ const AboutPrj = ({ role, tools, timeline, description, content, link }) => {
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <p className="font-normal text-xs leading-heading text-text-secondary">
+            <p className="font-bold text-xs leading-heading text-text-secondary">
               TOOLS
             </p>
             <p className="text-text-primary leading-body md:text-lg text-base font-medium w-fit">
@@ -24,7 +24,7 @@ const AboutPrj = ({ role, tools, timeline, description, content, link }) => {
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <p className="font-normal text-xs leading-heading text-text-secondary">
+            <p className="font-bold text-xs leading-heading text-text-secondary">
               TIMELINE
             </p>
             <p className="text-text-primary leading-body md:text-lg text-base font-medium">
@@ -33,7 +33,7 @@ const AboutPrj = ({ role, tools, timeline, description, content, link }) => {
           </div>
         </div>
         <div className="flex flex-col gap-2 md:pb-0 pb-6 md:w-[40%]">
-          <p className="font-normal text-xs leading-heading text-text-secondary">
+          <p className="font-bold text-xs leading-heading text-text-secondary">
             DESCRIPTION
           </p>
           <p className="text-text-primary leading-body md:text-lg text-base font-medium ">
@@ -42,7 +42,7 @@ const AboutPrj = ({ role, tools, timeline, description, content, link }) => {
         </div>
         <div className="flex flex-col md:gap-8 gap-2 md:w-[40%]">
           <div className="flex flex-col gap-2 md:pb-0 pb-6">
-            <p className="font-normal text-xs leading-heading text-text-secondary">
+            <p className="font-bold text-xs leading-heading text-text-secondary">
               CONTENT
             </p>
             <p className="text-text-primary leading-body md:text-lg text-base font-medium ">
