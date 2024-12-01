e75a9ec5411d5d221829549c05edc000d5f0b92c
harshii04 <harshvardhan.agarwal2020a@vitstudent.ac.in>
2024-12-01 17:24:49 +0530

    added new project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,7 @@
 import GFO from "./UI/Project Cards/GFO";
 import Dive from "./UI/Project Cards/Dive";
 import POA from "./UI/Project Cards/POA";
+import InvoiceFlo from "./UI/Project Cards/InvoiceFlo";
 
 const Project = () => {
   return (
@@ -12,9 +13,9 @@ const Project = () => {
           </p>
         </div>
         <div className="flex flex-col md:gap-8 gap-12">
+          <InvoiceFlo />
           <POA />
           <GFO />
-          <Dive />
         </div>
       </div>
     </>
---
+++ src/components/UI/Project Cards/InvoiceFlo.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import D01 from "../../../assets/ProjectImgs/InvoiceFlo/CoverImg.png";
+import ProjectInfo from "./ProjectInfo";
+
+const ProjectCard = () => {
+  return (
+    <>
+      <div className="">
+        <ProjectInfo
+          heading={"Crafting a Seamless Onboarding Journey for Business Owners"}
+          timeline={"Freelance project, Jul 2024"}
+          description={
+            "Designed an onboarding for MSME business owners using account aggregator and e-KYC."
+          }
+        />
+
+        <Link
+          href="https://bootcamp.uxdesign.cc/trivia-dive-intern-challenge-9bcefc186517"
+          target="_blank"
+          alt="Project link"
+        >
+          <div className=" bg-[#f4f6f8] hover:bg-[#E3EDFD] transition-all duration-500 ease-in-out md:py-12 md:px-8 py-8 px-4 flex flex-col gap-12 md:rounded-2xl rounded-lg">
+            <div>
+              <Image
+                src={D01}
+                alt="Project Image"
+                draggable={false}
+                className="md:rounded-2xl rounded-lg shadow-imgShadow"
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ProjectCard;
